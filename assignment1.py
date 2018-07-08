632eb1a69e297c778d1598cbb1ab73ba960e0efe
INSHAL123 <40802301+INSHAL123@users.noreply.github.com>
2018-07-08 13:36:03 +0530

    assignment 1

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+#1.Write a program take input from user and check whether it is and greater 50.
+x=int(input('enter a value'))
+print(x%2==0 and x>50)
+
+
+#2.Write a program take two float as a number and create calculator
+x=float(input('enter a value'))
+y=float(input('enter a value'))
+print('add is :',x+y)
+print('sub is :',x-y)
+print('mul is :',x*y)
+print('div is :',x/y)
+
+#3.Write a program take two strings from user and perform concatenation 'hello world'.
+x='hello '
+y='world'
+print(x+y)
+
+#4.Write a program take two input from user and check whether it is divisle by 17.
+x=int(input('enter a number'))
+print(x%17==0)   
